223a42e09dbdf783bab47d3715f58fc078dfb85c
Alejandro <alexvera0109@gmail.com>
2022-12-15 00:11:40 -0500

    added review filter, working on current page selection on sidenav

--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -2,22 +2,29 @@ import DashboardIcon from '@rsuite/icons/legacy/Dashboard'
 import GearCircleIcon from '@rsuite/icons/legacy/GearCircle'
 import GroupIcon from '@rsuite/icons/legacy/Group'
 import MagicIcon from '@rsuite/icons/legacy/Magic'
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import { Nav, Sidenav } from 'rsuite'
-
+import styles from './SideNav.module.css'
 const SideNav = () => {
+  const [selectedPage, setSelectedPage] = useState(0)
+
   return (
     <div style={{ width: 240 }}>
       <Sidenav defaultOpenKeys={['3', '4']} appearance='subtle'>
         <Sidenav.Body>
           <Nav activeKey='1'>
             <Nav.Item eventKey='1' icon={<DashboardIcon />}>
-              Home
+              <Link to={'/'} className={selectedPage !== 0 && styles.pageDisable} onClick={() => setSelectedPage(0)}>
+                Home
+              </Link>
             </Nav.Item>
             <Nav.Item eventKey='2' icon={<GroupIcon />}>
-              Search
+              <Link className={selectedPage !== 1 && styles.pageDisable} to={'/search'} onClick={() => setSelectedPage(0)}>
+                Search
+              </Link>
             </Nav.Item>
-            <Nav.Menu eventKey='3' title='Advanced' icon={<MagicIcon />}>
+            {/* <Nav.Menu eventKey='3' title='Advanced' icon={<MagicIcon />}>
               <Nav.Item eventKey='3-1'>Geo</Nav.Item>
               <Nav.Item eventKey='3-2'>Devices</Nav.Item>
               <Nav.Item eventKey='3-3'>Loyalty</Nav.Item>
@@ -31,7 +38,7 @@ const SideNav = () => {
                 <Nav.Item eventKey='4-5-1'>Action Name</Nav.Item>
                 <Nav.Item eventKey='4-5-2'>Action Params</Nav.Item>
               </Nav.Menu>
-            </Nav.Menu>
+            </Nav.Menu> */}
           </Nav>
         </Sidenav.Body>
       </Sidenav>
--- src/pages/Search/components/FilterBar.jsx
+++ src/pages/Search/components/FilterBar.jsx
@@ -1,6 +1,6 @@
 import SearchIcon from '@rsuite/icons/Search'
 import React from 'react'
-import { Divider, Input, InputGroup, InputPicker, Stack, TagPicker } from 'rsuite'
+import { Button, Divider, Input, InputGroup, InputPicker, Stack, TagPicker } from 'rsuite'
 
 const priceData = ['Under $2', 'Under $5', 'Under $10', 'Under $15', 'Under $20', 'Under $30', 'Above $30'].map((item, index) => ({
   label: item,
@@ -39,7 +39,10 @@ const storeData = [
   label: item,
   value: index,
 }))
-
+const reviewData = ['At least 40%', 'At least 50%', 'At least 60%', 'At least 70%', 'At least 80%', 'Greater than 80%'].map((item, index) => ({
+  label: item,
+  value: index,
+}))
 const filterBar = () => {
   return (
     <Stack divider={<Divider vertical />}>
@@ -52,6 +55,8 @@ const filterBar = () => {
       <InputPicker data={priceData} style={{ width: 175 }} placeholder='Price' />
       <InputPicker data={discountData} style={{ width: 175 }} placeholder='Discount' />
       <TagPicker data={storeData} style={{ width: 175 }} placeholder='Stores' />
+      <InputPicker data={reviewData} style={{ width: 175 }} placeholder='Discount' />
+      <Button>Search</Button>
     </Stack>
   )
 }
