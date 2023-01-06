459b1e70cf98d6791970071fc30c34dc6fc40d60
Alejandro <alexvera0109@gmail.com>
2023-01-06 15:53:47 -0500

    error in var passing

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import TooltipSlider from 'rc-slider'
 import 'rc-slider/assets/index.css'
 import React, { useState } from 'react'
@@ -6,20 +7,47 @@ import SideNav from '../../components/SideNav/SideNav'
 import FilterBar from './components/FilterBar'
 import FilteredDeals from './components/FilteredDeals'
 import styles from './Search.module.css'
-const Search = ({ price, discount, time, store, reviews, title, dealRating }) => {
+const Search = () => {
   const [sortTitle, setSortTitle] = useState('Lowest Price')
 
   const handleSelect = newVal => {
     setSortTitle(newVal)
   }
+  const [allDeals, setAllDeals] = useState(null)
 
+  const handleSearch = ({ lowerPrice }) => {
+    console.log(lowerPrice)
+    axios.get(`https://www.cheapshark.com/api/1.0/deals?metacritic=60&steamRating=70&sortBy=recent&lowerPrice=${lowerPrice}`).then(response => {
+      setAllDeals(response.data)
+      let newArr = []
+
+      for (let i = 0; i < response.data.length; i++) {
+        let isDup = false
+        if (newArr.length > 0) {
+          for (let j = 0; j < newArr.length; j++) {
+            if (response.data[i].internalName === newArr[j].internalName) {
+              isDup = true
+              break
+            }
+          }
+          if (isDup === false) {
+            newArr.push(response.data[i])
+          }
+        } else {
+          newArr.push(response.data[i])
+        }
+      }
+      setAllDeals(newArr)
+      console.log(newArr)
+    })
+  }
   return (
     <div className={styles.container}>
       <SideNav></SideNav>
       <div className={styles.mainStack}>
         <h1>Search game deals</h1>
         <hr />
-        <FilterBar />
+        <FilterBar handleSearch={() => handleSearch()} />
         <hr />
         <div className={styles.dealAndSortDiv}>
           <div className={styles.dealParent}>
--- src/pages/Search/components/FilterBar.jsx
+++ src/pages/Search/components/FilterBar.jsx
@@ -1,5 +1,5 @@
 import SearchIcon from '@rsuite/icons/Search'
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Divider, Input, InputGroup, InputPicker, Stack, TagPicker } from 'rsuite'
 import styles from './FilterBar.module.css'
 const priceData = ['Under $2', 'Under $5', 'Under $10', 'Under $15', 'Under $20', 'Under $30', 'Above $30'].map((item, index) => ({
@@ -43,7 +43,41 @@ const reviewData = ['At least 40%', 'At least 50%', 'At least 60%', 'At least 70
   label: item,
   value: index,
 }))
-const filterBar = () => {
+
+const FilterBar = ({ handleSearch }) => {
+  const [lowerPrice, setLowerPrice] = useState('50')
+  const [stores, setStores] = useState('')
+
+  const handlePriceChange = value => {
+    switch (value) {
+      case null:
+        setLowerPrice('50')
+        break
+      case 0:
+        setLowerPrice('2')
+        break
+      case 1:
+        setLowerPrice('5')
+        break
+      case 2:
+        setLowerPrice('10')
+        break
+      case 3:
+        setLowerPrice('15')
+        break
+      case 4:
+        setLowerPrice('20')
+        break
+      case 5:
+        setLowerPrice('30')
+        break
+      case 6:
+        setLowerPrice('120')
+        break
+      default:
+        console.log('error in handlePriceChange')
+    }
+  }
   return (
     <Stack divider={<Divider vertical />}>
       <InputGroup size='md' style={{ width: 224 }}>
@@ -52,13 +86,21 @@ const filterBar = () => {
           <SearchIcon />
         </InputGroup.Button>
       </InputGroup>
-      <InputPicker data={priceData} style={{ width: 175 }} placeholder='Price' />
+      <InputPicker data={priceData} style={{ width: 175 }} placeholder='Price' onChange={value => handlePriceChange(value)} />
       <InputPicker data={discountData} style={{ width: 175 }} placeholder='Discount' />
       <TagPicker data={storeData} style={{ width: 175 }} placeholder='Stores' />
       <InputPicker data={reviewData} style={{ width: 175 }} placeholder='Discount' />
-      <Button appearance='primary'>Search</Button>
+      <Button
+        appearance='primary'
+        onClick={() => {
+          console.log(lowerPrice)
+          handleSearch(lowerPrice)
+        }}
+      >
+        Search
+      </Button>
     </Stack>
   )
 }
 
-export default filterBar
+export default FilterBar
