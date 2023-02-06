6b0275c83064b42c943578972bc605e765dd8e72
Alejandro <alexvera0109@gmail.com>
2023-02-06 18:33:16 -0500

    Deal Rating Search Bug

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -2,6 +2,7 @@ import axios from 'axios'
 import TooltipSlider from 'rc-slider'
 import 'rc-slider/assets/index.css'
 import React, { useState } from 'react'
+import { AiOutlineFire } from 'react-icons/ai'
 import { Dropdown } from 'rsuite'
 import SideNav from '../../components/SideNav/SideNav'
 import FilterBar from './components/FilterBar'
@@ -9,13 +10,14 @@ import FilteredDeals from './components/FilteredDeals'
 import styles from './Search.module.css'
 const Search = () => {
   const [sortTitle, setSortTitle] = useState('Lowest Price')
-
   const handleSelect = newVal => {
     setSortTitle(newVal)
   }
   const [allDeals, setAllDeals] = useState(null)
-
+  const [slideRating, setSlideRating] = useState(5)
   const handleSearch = ({ title, lowerPrice, discount, stores, review }) => {
+    let slideDealRating = Math.abs(slideRating - 10)
+
     let newStoreArr = []
     if (stores !== null && stores.length !== 0) {
       for (let i = 0; i < stores.length; i++) {
@@ -25,7 +27,15 @@ const Search = () => {
     let storeList = newStoreArr.toString()
     let fetchLink = `https://www.cheapshark.com/api/1.0/deals?metacritic=60&sortBy=recent`
     console.log(stores)
-    console.log('Title: ' + title, 'lowerPrice: ' + lowerPrice, 'discount: ' + discount, 'stores: ' + storeList, 'review: ' + review)
+    console.log(
+      'Title: ' + title,
+      'lowerPrice: ' + lowerPrice,
+      'discount: ' + discount,
+      'stores: ' + storeList,
+      'review: ' + review,
+      'sortTitle: ' + sortTitle,
+      'slideRating: ' + slideDealRating
+    )
     if (lowerPrice !== null) {
       fetchLink = fetchLink.concat(`&upperPrice=${lowerPrice}`)
     }
@@ -37,6 +47,13 @@ const Search = () => {
     }
     if (title.length > 0) {
       fetchLink = fetchLink.concat(`&title=${title}`)
+    }
+    if (sortTitle.length !== null) {
+      if (sortTitle === 'Highest Price' || sortTitle === 'Lowest Price') {
+        fetchLink = fetchLink.concat(`&sortBy=Price`)
+      } else {
+        fetchLink = fetchLink.concat(`&sortBy=${sortTitle}`)
+      }
     }
     console.log('Fetch link: ' + fetchLink)
     axios.get(fetchLink).then(response => {
@@ -59,7 +76,16 @@ const Search = () => {
           newArr.push(response.data[i])
         }
       }
-      setAllDeals(newArr)
+      if (sortTitle === 'Highest Price') {
+        newArr.reverse()
+      }
+      let newerArr = newArr
+      for (let i = 0; i < newArr.length; i++) {
+        if (newArr[i].dealRating < slideDealRating) {
+          newerArr.push(i)
+        }
+      }
+      setAllDeals(newerArr)
       if (discount !== null) {
         let dealArr = []
         for (let i = 0; i < allDeals.length; i++) {
@@ -70,10 +96,11 @@ const Search = () => {
         console.log(dealArr)
         setAllDeals(dealArr)
       } else {
-        console.log(newArr)
+        console.log(newerArr)
       }
     })
   }
+
   return (
     <div className={styles.container}>
       <SideNav></SideNav>
@@ -84,18 +111,40 @@ const Search = () => {
         <hr />
         <div className={styles.dealAndSortDiv}>
           <div className={styles.dealParent}>
-            <span className={styles.whiteText}>Deal Rating</span>
-            <TooltipSlider className={styles.tooltipSlider} min={0} max={10} reverse defaultValue={5} />
+            <span className={styles.whiteText}>Deal Rating:</span>
+            <div className={styles.sliderParent}>
+              <div
+                className={
+                  Math.abs(slideRating - 10) >= 8
+                    ? styles.hotDealSlider
+                    : Math.abs(slideRating - 10) >= 6
+                    ? styles.warmDealSlider
+                    : styles.neutralDealSlider
+                }
+              >
+                &ge;{Math.abs(slideRating - 10)}
+                {Math.abs(slideRating - 10) >= 6 && <AiOutlineFire></AiOutlineFire>}
+              </div>
+              <TooltipSlider
+                className={styles.tooltipSlider}
+                min={0}
+                max={10}
+                reverse
+                defaultValue={5}
+                value={slideRating}
+                onChange={val => setSlideRating(val)}
+              />
+            </div>
           </div>
           <div className={styles.sortParent}>
             <span className={styles.whiteText}>Sort By: </span>
             <Dropdown title={sortTitle}>
               <Dropdown.Item onSelect={() => handleSelect('Lowest Price')}>Lowest Price</Dropdown.Item>
               <Dropdown.Item onSelect={() => handleSelect('Highest Price')}>Highest Price</Dropdown.Item>
-              <Dropdown.Item onSelect={() => handleSelect('Discount')}>Discount</Dropdown.Item>
+              <Dropdown.Item onSelect={() => handleSelect('Savings')}>Discount</Dropdown.Item>
               <Dropdown.Item onSelect={() => handleSelect('Deal Rating')}>Deal Rating</Dropdown.Item>
-              <Dropdown.Item onSelect={() => handleSelect('Latest Deals')}>Latest Deals</Dropdown.Item>
-              <Dropdown.Item onSelect={() => handleSelect('Metascore')}>Metascore</Dropdown.Item>
+              <Dropdown.Item onSelect={() => handleSelect('Recent')}>Latest Deals</Dropdown.Item>
+              <Dropdown.Item onSelect={() => handleSelect('Metacritic')}>Metascore</Dropdown.Item>
             </Dropdown>
           </div>
         </div>
