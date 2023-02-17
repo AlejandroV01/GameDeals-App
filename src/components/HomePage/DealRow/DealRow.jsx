e25c7c4c6b89bbaf576fb8aeaf6de95a142971a8
Alejandro <alexvera0109@gmail.com>
2023-02-17 12:30:56 -0500

    loader on games error

--- src/components/HomePage/DealRow/DealRow.jsx
+++ src/components/HomePage/DealRow/DealRow.jsx
@@ -35,37 +35,45 @@ const DealRow = ({ cheapSharkUrl }) => {
       setAllDeals(newArr)
     })
   }, [])
+  const loaderGames = () => {
+    // let htmlObject = document.createElement('div')
+    // for (let i = 0; i < 4; i++) {
+    //   htmlObject.innerHTML += <h1>Hello</h1>
+    // }
+    // return htmlObject
+  }
   return (
     <div className={styles.dealRow}>
-      {allDeals &&
-        allDeals.map((deal, index) => (
-          <div className={styles.card} key={index}>
-            <a target='_blank' rel='noreferrer' href={`https://www.cheapshark.com/redirect?dealID=${deal.dealID}`} className={styles.moreA}>
-              {}
-            </a>
-            <div className={styles.leftSide}>
-              <img
-                className={styles.headerImage}
-                src={deal.steamAppID ? `https://cdn.akamai.steamstatic.com/steam/apps/${deal.steamAppID}/header.jpg` : `${deal.thumb}`}
-                alt='a game img'
-              />
+      {allDeals
+        ? allDeals.map((deal, index) => (
+            <div className={styles.card} key={index}>
+              <a target='_blank' rel='noreferrer' href={`https://www.cheapshark.com/redirect?dealID=${deal.dealID}`} className={styles.moreA}>
+                {}
+              </a>
+              <div className={styles.leftSide}>
+                <img
+                  className={styles.headerImage}
+                  src={deal.steamAppID ? `https://cdn.akamai.steamstatic.com/steam/apps/${deal.steamAppID}/header.jpg` : `${deal.thumb}`}
+                  alt='a game img'
+                />
+              </div>
+              <div className={styles.rightSide}>
+                <h4 className={styles.gameHeader}>{deal.title}</h4>
+                <Stack divider={<Divider vertical />}>
+                  <div className={styles.dealText}>
+                    <span>{Math.round(deal.savings)}% Off </span>
+                  </div>
+                  <div>
+                    <img className={styles.storeLogo} src={`https://www.cheapshark.com/img/stores/logos/${deal.storeID - 1}.png`} alt='' />
+                  </div>
+                  <div className={`${deal.savings > 65 ? styles.goodDeal : styles.normalDeal} ${styles.dealText}`}>
+                    {deal.salePrice > 0 ? <span>${deal.salePrice}</span> : <span>FREE</span>}
+                  </div>
+                </Stack>
+              </div>
             </div>
-            <div className={styles.rightSide}>
-              <h4 className={styles.gameHeader}>{deal.title}</h4>
-              <Stack divider={<Divider vertical />}>
-                <div className={styles.dealText}>
-                  <span>{Math.round(deal.savings)}% Off </span>
-                </div>
-                <div>
-                  <img className={styles.storeLogo} src={`https://www.cheapshark.com/img/stores/logos/${deal.storeID - 1}.png`} alt='' />
-                </div>
-                <div className={`${deal.savings > 65 ? styles.goodDeal : styles.normalDeal} ${styles.dealText}`}>
-                  {deal.salePrice > 0 ? <span>${deal.salePrice}</span> : <span>FREE</span>}
-                </div>
-              </Stack>
-            </div>
-          </div>
-        ))}
+          ))
+        : loaderGames()}
       <div className={`${styles.card} ${styles.moreCard}`}>
         <Link to='/search' className={styles.moreA}></Link>
         <div className={styles.moreDiv}>
